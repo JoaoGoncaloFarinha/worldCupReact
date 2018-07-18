e8e7768b0eca5af60d7958be70a32457a54b4462
JoaoGoncaloFarinha <joaofarinha1994@gmail.com>
2018-07-18 21:57:13 +0100

    added searchbar and filter for countries

--- worldcup/src/App.js
+++ worldcup/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import {countries} from './countries.js';
-import CardList from './CardList'
+import CardList from './CardList';
+import SearchBox from './SearchBox';
 
-const App = () => {
-	return(<div>
-				<CardList countries={countries} />
-			</div>	
-	)
+class App extends Component{
+	constructor(){
+		super();
+		this.state = {
+			countries: countries,
+			searchField: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchField: event.target.value });
+	}
+
+	render(){
+		const filteredCountries = this.state.countries.filter( countries =>{
+			return countries.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+		})
+		console.log(filteredCountries)
+		return(
+			<div className='tc'>
+				<h1>WorldCup Teams</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList countries={filteredCountries} />
+			</div>
+		 );
+	}
 }
 
 export default App;
